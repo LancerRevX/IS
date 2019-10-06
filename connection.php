fa54a1d4eb4b17af8e6fb8eb4140f9ad7c3878ff
LancerRevX <50010225+LancerRevX@users.noreply.github.com>
2019-10-06 23:17:16 +0700

    1

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+    require("constants.php");
+
+    $con = mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());
+    mysql_select_db(DB_NAME) or die("Could not select database");
+    mysql_select_db
+ 
